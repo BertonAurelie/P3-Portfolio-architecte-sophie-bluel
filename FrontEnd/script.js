70364301643d9a27bf7c042afbbbcf892676a3d3
Berton Aurelie <lidingway>
2023-11-15 18:31:04 +0100

    ajout etape 1-1

---
+++ FrontEnd/script.js
@@ -0,0 +1,32 @@
+const tableaux = await fetch("http://localhost:5678/api/works");
+const pieces = await tableaux.json();
+console.log(pieces)
+
+async function afficherGallery(pieces) {
+    for (let i = 0; i < pieces.length; i++ ) {
+        let article = pieces[i];
+
+        // Récupération de l'élément du DOM(div gallery) qui accueillera les tableaux
+        const sectionGallery = document.querySelector(".gallery")
+        
+        // Création d’une balise dédiée à un projet
+        const projetElement = document.createElement ("projet")
+
+        // Création des balises qui composent le projet
+        const imageElement=document.createElement("img");
+        imageElement.src = article.imageUrl;
+        console.log(imageElement)
+
+        const titleElement=document.createElement("figcaption");
+        titleElement.innerText = article.title ?? "pas de titre";
+        console.log(titleElement)
+
+        sectionGallery.appendChild(projetElement);
+        projetElement.appendChild(imageElement);
+        projetElement.appendChild(titleElement);
+    } 
+
+} 
+ 
+afficherGallery(pieces)
+
